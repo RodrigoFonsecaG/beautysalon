4ba57a330fb2688e947337dad17d2a53ea537387
RodrigoFonsecaG <rodrigofon1@hotmail.com>
2021-09-23 09:53:53 -0300

    numero de clientes refatorado

--- modules/clients.js
+++ modules/clients.js
@@ -1,25 +1,33 @@
-export default function numerosClientes() {
-  const clientesP = document.querySelector('.numeros-clientes-text');
-  const funcionamento = document.querySelector('[data-semana]');
+export default class numerosClientes {
+  constructor(text, data) {
+    this.clientesP = document.querySelector(text);
+    this.funcionamento = document.querySelector(data);
+    this.numeroInicial = Math.floor(Math.random() * (50 - 10 + 1) + 10);
+  }
 
-  let numeroInicial = Math.floor(Math.random() * (50 - 10 + 1) + 10);
-
-  function addNumber(element) {
-    if (funcionamento.classList.contains('aberto')) {
+  addNumber(element) {
+    if (this.funcionamento.classList.contains('aberto')) {
+      //Se o estabelecimento estiver aberto executa a função
       setInterval(() => {
         if (document.documentElement.classList.contains('ptBR')) {
           element.innerHTML = `Número de clientes sendo atendidos no momento: ${
-            Math.random() < 0.5 ? numeroInicial++ : numeroInicial--
+            Math.random() < 0.5 ? this.numeroInicial++ : this.numeroInicial--
           }`;
         }
 
         if (document.documentElement.classList.contains('enUS')) {
           element.innerHTML = `Number of customers currently being attended: ${
-            Math.random() < 0.5 ? numeroInicial++ : numeroInicial--
+            Math.random() < 0.5 ? this.numeroInicial++ : this.numeroInicial--
           }`;
         }
       }, 2000);
     }
   }
-  addNumber(clientesP);
+
+  init() {
+    if (this.clientesP && this.funcionamento) {
+      this.addNumber(this.clientesP);
+    }
+    return this;
+  }
 }
