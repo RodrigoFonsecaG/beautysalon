f01927a0e07f8d1a38445ba46c380bef63bb7166
RodrigoFonsecaG <rodrigofon1@hotmail.com>
2021-10-16 12:33:33 -0300

    deletando funcionamento do agendamento

--- public/scripts/make-an-appointment.js
+++ public/scripts/make-an-appointment.js
@@ -3,6 +3,4 @@ import Appointment  from './modules/appointment-form.js';
 const appointment = new Appointment('.card', 'select[name="uf"]', 'select[name="city"]');
 appointment.init();
 
-import AppointmentFuncionamento from './modules/appointment-funcionamento.js';
-AppointmentFuncionamento();
 
